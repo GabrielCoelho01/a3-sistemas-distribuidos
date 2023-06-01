6e2ff1e2da74d0a30ae5f03e73d70b6d05a3ee26
GabrielCoelho01 <coelhogabriel12@gmail.com>
2023-06-01 20:58:13 -0300

    adicionando modo PVP

--- ImparOuPar/src/Client.java
+++ ImparOuPar/src/Client.java
@@ -2,87 +2,127 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        do {
-            playGame();
+        System.out.println("Selecione o modo de jogo:");
+        System.out.println("1. PvP");
+        System.out.println("2. PVE ");
 
-            System.out.println("Deseja jogar novamente? (s/n)");
-            String playAgain = scanner.nextLine();
-            if (!playAgain.equalsIgnoreCase("s")) {
-                break;
-            }
-        } while (true);
+        int mode = scanner.nextInt();
+        if (mode == 1) {
+            playPvP(scanner);
+        } else if (mode == 2) {
+            playAgainstComputer(scanner);
+        } else {
+            System.out.println("Modo inválido.");
+        }
 
         scanner.close();
     }
 
-    private static void playGame() throws IOException {
+    private static void playPvP(Scanner scanner) throws IOException {
+        Socket player1Socket = new Socket("localhost", 3000);
+        System.out.println("Conectado como Jogador 1!");
+        ObjectOutputStream player1OutputStream = new ObjectOutputStream(player1Socket.getOutputStream());
+        ObjectInputStream player1InputStream = new ObjectInputStream(player1Socket.getInputStream());
+
+        System.out.print("Digite o endereço IP do Jogador 2: ");
+        String player2IP = scanner.next();
+        System.out.print("Digite a porta do Jogador 2: ");
+        int player2Port = scanner.nextInt();
+
+        Socket player2Socket = new Socket(player2IP, player2Port);
+        System.out.println("Conectado como Jogador 2!");
+        ObjectOutputStream player2OutputStream = new ObjectOutputStream(player2Socket.getOutputStream());
+        ObjectInputStream player2InputStream = new ObjectInputStream(player2Socket.getInputStream());
+
+        playGame(scanner, player1OutputStream, player1InputStream, player2OutputStream, player2InputStream);
+
+        player1Socket.close();
+        player2Socket.close();
+    }
+
+    private static void playAgainstComputer(Scanner scanner) throws IOException {
         Socket socket = new Socket("localhost", 3000);
         System.out.println("Conectado ao servidor!");
         ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-        Scanner scanner = new Scanner(System.in);
-        socket.setSoTimeout(5000); // Define o tempo limite de leitura para 5 segundos
 
-        int pontuacaoJogador = 0;
-        int pontuacaoOponente = 0;
+        playGame(scanner, outputStream, inputStream, outputStream, inputStream);
+
+        socket.close();
+    }
+
+    private static void playGame(Scanner scanner, ObjectOutputStream outputStream1, ObjectInputStream inputStream1,
+                                 ObjectOutputStream outputStream2, ObjectInputStream inputStream2) throws IOException {
+        int pontuacaoJogador1 = 0;
+        int pontuacaoJogador2 = 0;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Rodada " + (i + 1));
-            int escolhaJogador = 0;
-            while (escolhaJogador != 1 && escolhaJogador != 2) {
-                System.out.println("Jogador, escolha ímpar ou par (1 para ímpar, 2 para par):");
-                escolhaJogador = scanner.nextInt();
-                if (escolhaJogador != 1 && escolhaJogador != 2) {
-                    System.out.println("Escolha inválida. Por favor, escolha 1 para ímpar ou 2 para par.");
-                }
-            }
-            System.out.println("Escolha um número:");
-            int numeroJogador = scanner.nextInt();
-            outputStream.writeInt(escolhaJogador);
-            outputStream.writeInt(numeroJogador);
-            outputStream.flush();
+
+            int escolhaJogador1 = getPlayerChoice(scanner, 1);
+            int numeroJogador1 = getPlayerNumber(scanner);
+
+            outputStream1.writeInt(escolhaJogador1);
+            outputStream1.writeInt(numeroJogador1);
+            outputStream1.flush();
+
+            int escolhaJogador2 = getPlayerChoice(scanner, 2);
+            int numeroJogador2 = getPlayerNumber(scanner);
+
+            outputStream2.writeInt(escolhaJogador2);
+            outputStream2.writeInt(numeroJogador2);
+            outputStream2.flush();
 
             try {
-                int escolhaOponente = inputStream.readInt();
-                int numeroOponente = inputStream.readInt();
-                System.out.println("O oponente escolheu o número " + numeroOponente);
-                String resultado = inputStream.readUTF();
-                System.out.println(resultado);
+                String resultado1 = inputStream1.readUTF();
+                System.out.println("Jogador 1: " + resultado1);
 
-                if (resultado.equals("Você ganhou!")) {
-                    pontuacaoJogador++;
-                } else if (resultado.equals("Oponente ganhou!")) {
-                    pontuacaoOponente++;
+                String resultado2 = inputStream2.readUTF();
+                System.out.println("Jogador 2: " + resultado2);
+
+                if (resultado1.equals("Você ganhou!")) {
+                    pontuacaoJogador1++;
+                } else if (resultado2.equals("Você ganhou!")) {
+                    pontuacaoJogador2++;
                 }
-            } catch (SocketTimeoutException e) {
-                System.out.println("Tempo limite de leitura atingido. Continuando para a próxima rodada.");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        pontuacaoJogador = inputStream.readInt();
-        pontuacaoOponente = inputStream.readInt();
-
         System.out.println("Fim do jogo!");
-        System.out.println("Pontuação do jogador: " + pontuacaoJogador);
-        System.out.println("Pontuação do oponente: " + pontuacaoOponente);
-        if (pontuacaoJogador > pontuacaoOponente) {
-            System.out.println("Você venceu o jogo!");
-        } else if (pontuacaoOponente > pontuacaoJogador) {
-            System.out.println("Oponente venceu o jogo!");
+        System.out.println("Pontuação do Jogador 1: " + pontuacaoJogador1);
+        System.out.println("Pontuação do Jogador 2: " + pontuacaoJogador2);
+
+        if (pontuacaoJogador1 > pontuacaoJogador2) {
+            System.out.println("Jogador 1 venceu o jogo!");
+        } else if (pontuacaoJogador2 > pontuacaoJogador1) {
+            System.out.println("Jogador 2 venceu o jogo!");
         } else {
             System.out.println("Empate!");
         }
+    }
 
-        socket.close();
-        scanner.nextLine(); // Consume the newline character after the number input
+    private static int getPlayerChoice(Scanner scanner, int playerNumber) {
+        int escolhaJogador = 0;
+        while (escolhaJogador != 1 && escolhaJogador != 2) {
+            System.out.println("Jogador " + playerNumber + ", escolha ímpar ou par (1 para ímpar, 2 para par):");
+            escolhaJogador = scanner.nextInt();
+            if (escolhaJogador != 1 && escolhaJogador != 2) {
+                System.out.println("Escolha inválida. Por favor, escolha 1 para ímpar ou 2 para par.");
+            }
+        }
+        return escolhaJogador;
+    }
+
+    private static int getPlayerNumber(Scanner scanner) {
+        System.out.println("Escolha um número:");
+        return scanner.nextInt();
     }
 }
--- ImparOuPar/src/Server.java
+++ ImparOuPar/src/Server.java
@@ -10,76 +10,117 @@ public class Server {
         System.out.println("Servidor iniciado, aguardando conexões...");
 
         while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Cliente conectado: " + socket.getInetAddress());
+            Socket player1Socket = serverSocket.accept();
+            System.out.println("Jogador 1 conectado: " + player1Socket.getInetAddress());
 
-            Thread thread = new Thread(new PlayerThread(socket));
+            Socket player2Socket = serverSocket.accept();
+            System.out.println("Jogador 2 conectado: " + player2Socket.getInetAddress());
+
+            Thread thread = new Thread(new GameThread(player1Socket, player2Socket));
             thread.start();
         }
     }
 
-    private static class PlayerThread implements Runnable {
-        private final Socket socket;
+    private static class GameThread implements Runnable {
+        private final Socket player1Socket;
+        private final Socket player2Socket;
 
-        public PlayerThread(Socket socket) {
-            this.socket = socket;
+        public GameThread(Socket player1Socket, Socket player2Socket) {
+            this.player1Socket = player1Socket;
+            this.player2Socket = player2Socket;
         }
 
         @Override
         public void run() {
             try {
-                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream player1OutputStream = new ObjectOutputStream(player1Socket.getOutputStream());
+                ObjectInputStream player1InputStream = new ObjectInputStream(player1Socket.getInputStream());
 
-                int playerScore = 0;
-                int opponentScore = 0;
+                ObjectOutputStream player2OutputStream = new ObjectOutputStream(player2Socket.getOutputStream());
+                ObjectInputStream player2InputStream = new ObjectInputStream(player2Socket.getInputStream());
 
-                for (int i = 0; i < 3; i++) {
-                    int playerChoice = inputStream.readInt();
-                    int playerNumber = inputStream.readInt();
+                playGame(player1OutputStream, player1InputStream, player2OutputStream, player2InputStream);
 
-                    int opponentChoice = 3 - playerChoice; // Determine opponent's choice (odd or even)
-                    int opponentNumber = (int) (Math.random() * 6) + 1;
-
-                    outputStream.writeInt(opponentChoice);
-                    outputStream.writeInt(opponentNumber);
-                    outputStream.flush();
-
-                    int sum = playerChoice + playerNumber + opponentChoice + opponentNumber;
-                    String result;
-
-                    if (sum % 2 == 0) {
-                        result = "Par. ";
-                        if (playerChoice == 2) {
-                            result += "Você ganhou!";
-                            playerScore++;
-                        } else {
-                            result += "Oponente ganhou!";
-                            opponentScore++;
-                        }
-                    } else {
-                        result = "ímpar. ";
-                        if (playerChoice == 1) {
-                            result += "Você ganhou!";
-                            playerScore++;
-                        } else {
-                            result += "Oponente ganhou!";
-                            opponentScore++;
-                        }
-                    }
-
-                    outputStream.writeUTF(result);
-                    outputStream.flush();
-                }
-
-                outputStream.writeInt(playerScore);
-                outputStream.writeInt(opponentScore);
-                outputStream.flush();
-
-                socket.close();
+                player1Socket.close();
+                player2Socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        private void playGame(ObjectOutputStream player1OutputStream, ObjectInputStream player1InputStream,
+                              ObjectOutputStream player2OutputStream, ObjectInputStream player2InputStream) throws IOException {
+            int pontuacaoJogador1 = 0;
+            int pontuacaoJogador2 = 0;
+
+            for (int i = 0; i < 3; i++) {
+                int escolhaJogador1 = player1InputStream.readInt();
+                int numeroJogador1 = player1InputStream.readInt();
+
+                int escolhaJogador2 = player2InputStream.readInt();
+                int numeroJogador2 = player2InputStream.readInt();
+
+                player1OutputStream.writeInt(escolhaJogador2);
+                player1OutputStream.writeInt(numeroJogador2);
+                player1OutputStream.flush();
+
+                player2OutputStream.writeInt(escolhaJogador1);
+                player2OutputStream.writeInt(numeroJogador1);
+                player2OutputStream.flush();
+
+                int sum = escolhaJogador1 + numeroJogador1 + escolhaJogador2 + numeroJogador2;
+                String resultadoJogador1;
+                String resultadoJogador2;
+
+                if (sum % 2 == 0) {
+                    resultadoJogador1 = "Par. ";
+                    resultadoJogador2 = "Par. ";
+                    if (escolhaJogador1 == 2) {
+                        resultadoJogador1 += "Você ganhou!";
+                        pontuacaoJogador1++;
+                    } else {
+                        resultadoJogador1 += "Jogador 2 ganhou!";
+                        pontuacaoJogador2++;
+                    }
+                    if (escolhaJogador2 == 2) {
+                        resultadoJogador2 += "Você ganhou!";
+                        pontuacaoJogador2++;
+                    } else {
+                        resultadoJogador2 += "Jogador 1 ganhou!";
+                        pontuacaoJogador1++;
+                    }
+                } else {
+                    resultadoJogador1 = "ímpar. ";
+                    resultadoJogador2 = "ímpar. ";
+                    if (escolhaJogador1 == 1) {
+                        resultadoJogador1 += "Você ganhou!";
+                        pontuacaoJogador1++;
+                    } else {
+                        resultadoJogador1 += "Jogador 2 ganhou!";
+                        pontuacaoJogador2++;
+                    }
+                    if (escolhaJogador2 == 1) {
+                        resultadoJogador2 += "Você ganhou!";
+                        pontuacaoJogador2++;
+                    } else {
+                        resultadoJogador2 += "Jogador 1 ganhou!";
+                        pontuacaoJogador1++;
+                    }
+                }
+
+                player1OutputStream.writeUTF(resultadoJogador1);
+                player1OutputStream.flush();
+                player2OutputStream.writeUTF(resultadoJogador2);
+                player2OutputStream.flush();
+            }
+
+            player1OutputStream.writeInt(pontuacaoJogador1);
+            player1OutputStream.writeInt(pontuacaoJogador2);
+            player1OutputStream.flush();
+
+            player2OutputStream.writeInt(pontuacaoJogador2);
+            player2OutputStream.writeInt(pontuacaoJogador1);
+            player2OutputStream.flush();
         }
     }
 }
